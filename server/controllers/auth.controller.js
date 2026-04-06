b9ae54b0238d04bce29f84a131f76b40fd07d9ac
Rohit <rohitgupta1938@gmail.com>
2026-04-07 02:23:13 +0530

    Ai-ExamNotes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -13,7 +13,7 @@ export const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "None",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
