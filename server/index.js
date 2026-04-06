cbec352cbb10cd6997e6ebc9a09995934b87cc15
Rohit <rohitgupta1938@gmail.com>
2026-04-06 19:16:23 +0530

    Ai-ExamNotes

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app=express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://studynotesclient.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
